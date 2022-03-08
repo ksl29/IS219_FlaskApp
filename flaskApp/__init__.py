4f8781267c9003393b2396f11d313c0acb9f11e4
ksl29 <ksl29@njit.edu>
2022-03-07 21:06:05 -0500

    copied 404 fix

--- flaskApp/__init__.py
+++ flaskApp/__init__.py
@@ -3,6 +3,11 @@ from flask import Flask
 from flask import render_template
 from flaskApp import db, auth, blog, simple_pages
 from flaskApp.context_processors import utility_text_processors
+from werkzeug.exceptions import NotFound
+
+def page_not_found(e):
+    return render_template("404.html"),404
+
 
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
@@ -35,6 +40,7 @@ def create_app(test_config=None):
     app.register_blueprint(auth.bp)
     app.register_blueprint(blog.bp)
     app.register_blueprint(simple_pages.bp)
+    app.register_error_handler(404,page_not_found)
 
 
     # make url_for('index') == url_for('blog.index')
@@ -52,9 +58,3 @@ def create_app(test_config=None):
 
 
 app = create_app()
-
-@app.errorhandler(404)
-# inbuilt function which takes error as parameter
-def not_found(e):
-    # defining function
-    return render_template("404.html")
